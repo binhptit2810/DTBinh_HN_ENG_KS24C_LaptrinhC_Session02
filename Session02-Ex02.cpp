ee27759f2ceb843e71cb48052a8036b44b795d50
binhptit2810 <binhbadboy2810@gmail.com>
2024-11-20 22:08:00 +0700

    Upload bài tập session 02-Ex02

---
+++ Session02-Ex02.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// Khai bao so nguyen thu nhat
+	int firstNumber = 10;
+	
+	// Khai bao so nguyen thu hai
+	int secondNumber = 20;
+	
+	// Khai bao tong cua hai so nguyen
+	int total = firstNumber + secondNumber;
+	
+	// Tong cua hai so nguyen
+	printf ("Tong cua hai so nguyen la: %d\n", total);
+	
+	return 0;
+	
+}
