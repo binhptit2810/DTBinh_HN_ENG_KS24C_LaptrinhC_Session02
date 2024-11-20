a82f23c891f42f2bb18f8d0be8bb8b8843e25722
binhptit2810 <binhbadboy2810@gmail.com>
2024-11-20 22:57:20 +0700

    Upload bài tập session 02-Ex05

---
+++ session 02-Ex05.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	// Khai bao chieu dai cua hinh chu nhat
+	int chieudai = 15;
+	
+	// Khai bao chieu rong cua hinh chu nhat
+	int chieurong = 5;
+	
+	// Khai bao chu vi cua hinh chu nhat
+	int chuvi = (chieudai + chieurong) * 2;
+	
+	// Khai bao dien tich cua hinh chu nhat
+	int dientich = chieudai * chieurong;
+	
+	// Chu vi cua hinh chu nhat la
+	printf("Chu vi cua hinh chu nhat la:%d\n", chuvi);
+	
+	// Dien tich cua hinh chu nhat la
+	printf("Dien tich cua hinh chu nhat la:%d\n", dientich);
+	
+	return 0;
+	
+}
