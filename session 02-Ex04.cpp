c816e5d413cebd90063dffae68033e5247a4640b
binhptit2810 <binhbadboy2810@gmail.com>
2024-11-20 22:44:43 +0700

    Upload bài tập session 02-Ex04

---
+++ session 02-Ex04.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	// Khai bao bien canh cua hinh vuong
+	int canh = 10;
+	
+	// Khai bao chu vi cua hinh vuong
+	int chuvi = canh * 4;
+	
+	// Khai bao dien tich cua hinh vuong 
+	int dientich = canh * canh;
+	
+	// Chu vi cua hinh vuong la
+	printf("Chu vi cua hinh vuong la:%d\n", chuvi);
+	
+	// Dien tich cua hinh vuong la
+	printf("Dien tich cua hinh vuong la:%d\n", dientich);
+	
+	return 0;
+}
