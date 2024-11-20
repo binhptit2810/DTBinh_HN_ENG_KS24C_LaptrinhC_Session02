44cd140ca6e83f4a31f2db54dced8c19165d6aeb
binhptit2810 <binhbadboy2810@gmail.com>
2024-11-20 22:30:18 +0700

    Upload bài tập session 02-Ex03

---
+++ session 02-Ex03.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+	// Khai bao so nguyen thu nhat
+	int firstNumber = 20;
+	
+	// Khai bao so nguyen thu hai
+	int secondNumber = 10;
+	
+	// Khai bao tong cua hai so nguyen
+	int tong = firstNumber + secondNumber;
+	
+	// Khai bao hieu cua hai so nguyen
+	int hieu = firstNumber - secondNumber;
+	
+	// Khai bao tich cua hai so nguyen
+	int tich = firstNumber * secondNumber;
+	
+	// Khai bao thuong cua hai so nguyen
+	int thuong = firstNumber / secondNumber;
+	
+	// Tong cua hai so nguyen la
+	printf ("Tong cua hai so nguyen la:%d\n", tong);
+	
+	// Hieu cua hai so nguyen la
+	printf ("Hieu cua hai so nguyen la:%d\n", hieu);
+	
+	// Tich cua hai so nguyen la
+	printf ("Tich cua hai so nguyen la:%d\n", tich);
+	
+	// Thuong cua hai so nguyen la
+	printf ("Thuong cua hai so nguyen la:%d\n", thuong);
+	
+	return 0;
+	
+}
